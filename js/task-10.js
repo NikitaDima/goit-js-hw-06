8456572bccdcadae3707cd40a1b5c867df8c1278
NikitaDima <onekill13@gmail.com>
2023-02-07 00:53:02 +0200

    работа над 10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,20 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const parentsEl = document.querySelector('#controls');
+const btnDestroy = parentsEl.lastElementChild;
+const btnCreate = btnDestroy.previousElementSibling;
+const inputEl = parentsEl.firstElementChild;
+const boxesConteiner = document.querySelector('#boxes');
+
+btnCreate.addEventListener('click', () => {
+  
+  for(let i = 1; i <= inputEl.value; i += 1) {
+    const addDiv = document.createElement('div');
+    
+  }
+})
+
+
+
