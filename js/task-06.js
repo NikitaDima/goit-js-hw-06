c49e6c753aec59358205b46f7b317677a0cf7b32
NikitaDima <onekill13@gmail.com>
2023-02-09 01:47:45 +0200

    правки від ментора

--- js/task-06.js
+++ js/task-06.js
@@ -2,10 +2,11 @@ const inputEl = document.querySelector('#validation-input');
 inputEl.addEventListener('blur', (event) => {
     inputEl.classList.remove('valid', 'invalid');
 
-    if(inputEl.value.length >= inputEl.dataset.length) {
+    if(inputEl.value.length >= Number(inputEl.dataset.length)) {
         return inputEl.classList.add('valid');
     } 
    
     return inputEl.classList.add('invalid');   
     
-})
+});
+
