192d11471dfb426ff3ac912b48a08a2d01b40b80
NikitaDima <onekill13@gmail.com>
2023-02-10 07:12:36 +0200

    10...

--- js/task-10.js
+++ js/task-10.js
@@ -8,13 +8,26 @@ const btnCreate = btnDestroy.previousElementSibling;
 const inputEl = parentsEl.firstElementChild;
 const boxesConteiner = document.querySelector('#boxes');
 
-btnCreate.addEventListener('click', () => {
-  
-  for(let i = 1; i <= inputEl.value; i += 1) {
-    const addDiv = document.createElement('div');
-    
-  }
-})
 
+function createBoxes(amout) {
+  amout = inputEl.value;
+   for(let i = 1; i <= inputEl.value; i += 1) {
+    const addDivEl = document.createElement('div');
+    addDivEl.style.background = getRandomHexColor();
+    addDivEl.style.width = `${30 + i * 10}px`;
+    addDivEl.style.height = `${30 + i * 10}px`;
+    boxesConteiner.append(addDivEl);
+  }
+};
+
+function destroyBoxes() {
+  inputEl.value = '';
+  const removeEl = boxesConteiner.children;
+  
+}
+
+
+btnCreate.addEventListener('click', createBoxes);
+btnDestroy.addEventListener('click',destroyBoxes);
 
 
